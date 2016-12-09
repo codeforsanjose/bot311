25b76bc2ad3f24be1e5d39432cbfd6c7c87f9ed3
Clarence Leung <hello@clarle.com>
2016-12-08 16:20:24 -0800

    Quick test of HeroCards

--- dialogs/nearby.js
+++ dialogs/nearby.js
@@ -1,5 +1,19 @@
+const builder = require('botbuilder');
+
 module.exports = [
     function (session) {
-        session.send("TODO: Build the nearby issues dialog!");
+        const card = new builder.HeroCard(session)
+            .title('Graffiti')
+            .subtitle('Intersection of Fillmore St & Geary Blvd')
+            .text('1750 Geary at Fillmore, southeast side up from corner. Graffiti on sidewalk. Offensive. Thanks.')
+            .images([
+                builder.CardImage.create(session, "http://mobile311-dev.sfgov.org/media/san_francisco/report/photos/58477e41ff035c2789f7ef7d/photo_20161206_191240.jpg")
+            ]);
+
+        const message = new builder.Message(session)
+            .attachmentLayout(builder.AttachmentLayout.carousel)
+            .attachments([card, card, card]);
+
+        session.send(message);
     }
 ];
